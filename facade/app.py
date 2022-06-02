5134f622ca7e1e212714c5fa58be62c874328c9f
Pavlo Semchyshyn <pavlosemchyshyn@gmail.com>
2022-06-02 22:35:14 +0300

    added queue imp

--- facade/app.py
+++ facade/app.py
@@ -2,11 +2,12 @@ from flask import Flask, request
 import uuid
 import requests
 import os
+import random
 
 
 os.environ['NO_PROXY'] = '127.0.0.1'
-LOGGING_SERVICE = "http://127.0.0.1:5001"
-MESSAGE_SERVICE = "http://127.0.0.1:5002"
+LOGGING_SERVICES = ["http://127.0.0.1:5002", "http://127.0.0.1:5003", "http://127.0.0.1:5004"]
+MESSAGE_SERVICE = "http://127.0.0.1:5001"
 app = Flask(__name__)
 
 
@@ -14,7 +15,8 @@ app = Flask(__name__)
 def save():
     uid = str(uuid.uuid4())
     try:
-        requests.post(LOGGING_SERVICE + '/save', json={uid: request.json})
+        logging_service = LOGGING_SERVICES[random.randint(0, 2)]
+        requests.post(logging_service + '/save', json={uid: request.json})
     except Exception as e:
         return f"Unable to save: {e}"
     return "Saved successfully"
@@ -25,11 +27,16 @@ def retrieve():
     '''
     Retrieves data from logging and messages services as an concatenated string
     '''
-    try:
-        result_log = requests.get(LOGGING_SERVICE + '/retrieve', json=request.json)
-        result_mes = requests.get(MESSAGE_SERVICE + '/retrieve', json=request.json)
-    except Exception as e:
-        return f"Unable to get data from servies: {e}"
+    result_log = result_mes = None
+    for service in LOGGING_SERVICES:
+        try:
+            result_log = requests.get(service + '/retrieve', json=request.json)
+            result_mes = requests.get(MESSAGE_SERVICE + '/retrieve', json=request.json)
+            break
+        except Exception as e:
+            print(f"Unable to get data from service: {e}")
+    if result_log is None or result_mes is None:
+        return ""
     return result_log.text + '|' + result_mes.text
 
 
--- logging/app.py
+++ logging/app.py
@@ -1,14 +1,19 @@
+import hazelcast
 from flask import Flask, request
 
 DATA = {}
 app = Flask(__name__)
 
 
+client = hazelcast.HazelcastClient()
+map = client.get_map("map-lock")
+
+
 @app.route('/save', methods=["POST"])
 def save():
     for k, v in request.json.items():
         try:
-            DATA[k] = v
+            map.put(k, v).result()
             print(f"Added to hashmap key: {k}, value: {v}")
         except Exception as e:
             return f"Unable to save key {k} with value {v}", 503
@@ -17,7 +22,7 @@ def save():
 
 @app.route('/retrieve', methods=["GET"])
 def retrieve():
-    return ",".join(DATA.values())
+    return ",".join(map.values().result())
 
 
 if __name__ == '__main__':
