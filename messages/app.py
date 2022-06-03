fc46ef24446ea798b7c425e03e93110fb0e8d3c8
Pavlo Semchyshyn <pavlosemchyshyn@gmail.com>
2022-06-03 19:36:11 +0300

    finish

--- messages/app.py
+++ messages/app.py
@@ -1,12 +1,30 @@
 from flask import Flask
-
+import hazelcast
+import threading
+import sys
 
 app = Flask(__name__)
+MESSAGES = []
+
+
+def polling():
+    client = hazelcast.HazelcastClient()
+    q = client.get_queue("message-queue")
+    while True:
+        v = q.poll().result()
+        if v is None:
+            continue
+        print(f"Got: {v}", flush=True)
+        MESSAGES.append(v)
 
 
 @app.route("/retrieve", methods=["GET"])
 def retrieve():
-    return "Not implemented yet"
+    return ",".join(MESSAGES)
+
+
+t = threading.Thread(target=polling)
+t.start()
 
 
 if __name__ == '__main__':
