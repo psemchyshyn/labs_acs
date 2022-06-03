93f93caa00da21ef6092859509034a08a9ac3648
Pavlo Semchyshyn <pavlosemchyshyn@gmail.com>
2022-06-03 19:35:47 +0300

    finish messagin

--- facade/app.py
+++ facade/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request
 import uuid
+import hazelcast
 import requests
 import os
 import random
 
 
 os.environ['NO_PROXY'] = '127.0.0.1'
-LOGGING_SERVICES = ["http://127.0.0.1:5002", "http://127.0.0.1:5003", "http://127.0.0.1:5004"]
-MESSAGE_SERVICE = "http://127.0.0.1:5001"
+LOGGING_SERVICES = ["http://127.0.0.1:5003", "http://127.0.0.1:5004", "http://127.0.0.1:5005"]
+MESSAGE_SERVICES = ["http://127.0.0.1:5001", "http://127.0.0.1:5001"]
 app = Flask(__name__)
+
+client = hazelcast.HazelcastClient()
+q = client.get_queue("message-queue")
 
 
 @app.route('/save', methods=["POST"])
@@ -16,6 +20,7 @@ def save():
     uid = str(uuid.uuid4())
     try:
         logging_service = LOGGING_SERVICES[random.randint(0, 2)]
+        q.put(request.json).result()
         requests.post(logging_service + '/save', json={uid: request.json})
     except Exception as e:
         return f"Unable to save: {e}"
@@ -27,17 +32,25 @@ def retrieve():
     '''
     Retrieves data from logging and messages services as an concatenated string
     '''
-    result_log = result_mes = None
-    for service in LOGGING_SERVICES:
+    result_log = result_mes = ""
+    random.shuffle(LOGGING_SERVICES)
+    random.shuffle(MESSAGE_SERVICES)
+
+    for log_service in LOGGING_SERVICES:
         try:
-            result_log = requests.get(service + '/retrieve', json=request.json)
-            result_mes = requests.get(MESSAGE_SERVICE + '/retrieve', json=request.json)
+            result_log = requests.get(log_service + '/retrieve', json=request.json).text
             break
         except Exception as e:
-            print(f"Unable to get data from service: {e}")
-    if result_log is None or result_mes is None:
-        return ""
-    return result_log.text + '|' + result_mes.text
+            print(f"Unable to get data from logging service: {e}")
+
+    for mes_service in MESSAGE_SERVICES:
+        try:
+            result_mes = requests.get(mes_service + '/retrieve', json=request.json).text
+            break
+        except Exception as e:
+            print(f"Unable to get data from message service: {e}")
+
+    return result_log + '|' + result_mes
 
 
 if __name__ == '__main__':
